c6c22998e50a0dbe423277b5cf54972d991320f0
pushiqiang <pushiqiang@126.com>
2017-07-19 19:27:55 +0800

    去掉自动化部署脚本

--- docker/README.md
+++
@@ -1,8 +0,0 @@
-# 快速部署指南
-
-## 运行开发环境
-
-    ./start_develop.sh
-
-如果docker image 修改了，选择重建镜像
-
--- docker/flask/pip_install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-pip install -i https://pypi.doubanio.com/simple  \
-        -r /opt/django_deploy/requirements.txt --trusted-host pypi.doubanio.com
